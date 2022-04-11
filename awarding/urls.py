edbad93ca18318a4735d9a514a46400bce13535e
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-11 21:55:52 +0300

    Creating the profile model

---
+++ awarding/urls.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phone_field import PhoneField
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    bio = models.TextField(max_length=400, blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = models.ImageField(upload_to='images/',default='https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png')
+    phone_number = PhoneField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
