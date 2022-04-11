375601533ba176c2019ab8d8c7d349c47a001a1a
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-11 22:01:46 +0300

    Creating the User Registrtion Form

--- awarding/views.py
+++ awarding/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http  import HttpResponseRedirect,Http404
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post, Profile,Rating
+import random
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
